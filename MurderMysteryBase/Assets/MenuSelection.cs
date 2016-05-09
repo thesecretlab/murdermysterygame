e6d25d1246ae02c3c2401255a6dd2f6639818b4b
whoaie <nickstevenson1994@gmail.com>
2016-05-09 17:14:10 +1000

    Map and Menu
    The map and menu scenes and scripts.

---
+++ MurderMysteryBase/Assets/MenuSelection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSelection : MonoBehaviour
+{
+    public bool newGame;
+    public bool options;
+    public bool quit;
+    // Use this for initialization
+
+    void OnMouseUp()
+    {
+        //Currently both options select the map screen, easy to change.
+        if (newGame)
+        {
+            Application.LoadLevel(1);
+        }
+       
+        if (options)
+        {
+            Application.LoadLevel(1);
+        }
+        if (quit)
+        {
+            Application.Quit();
+        }
+    }
+}
