a8354a28fb92ea106e93862062dfdee9678a1387
syhrus <tugman1234@hotmail.com>
2016-09-17 18:33:20 +1000

    Added connections to Morgue
    Scripts are in, but need to add bits to dialogue.

---
+++ MurderMysteryBase/Assets/CallEventScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Yarn.Unity;
+using Yarn.Unity.Example;
+
+public class CallEventScript : MonoBehaviour {
+
+    public bool callDone = false;
+
+    private DialogueRunner dialogueRunner;
+
+	public void startCall()
+    {
+        callDone = true;
+    }
+
+    void Start()
+    {
+        dialogueRunner = GameObject.Find("DialogueObject").GetComponentInChildren<DialogueRunner>();
+    }
+
+    void Update()
+    {
+        if (callDone)
+        {
+            callDone = false;
+            dialogueRunner.StartDialogue("MorticianCall.Call");
+        }
+    }
+	
+}
